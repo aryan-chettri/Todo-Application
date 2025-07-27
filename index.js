cd0d794db8b3006368f3141ce2671959afe6419a
Aryan Chettri <aryanchettri17@gmail.com>
2025-07-27 10:46:34 +0530

    The Backend Part. However the functionality is static

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function getCurrentDate(){
     
     // document.querySelector('.currentDate').innerHTML= now.toLocaleString();
 
-    return now.toLocaleDateString()
+    return now.toLocaleDateString();
 }
 
 let currentDate = getCurrentDate();
@@ -24,3 +24,44 @@ let currentDate = getCurrentDate();
 document.querySelector('.currentDate').innerHTML = currentDate ; 
 
 getCurrentDay();
+
+
+document.querySelector('.button-CreateABlog').addEventListener('click', function(){
+    const element = document.createElement('input');
+    element.className = 'input';
+    const button = document.createElement('button');
+    
+    button.className = 'submitButton'
+    button.textContent = 'Submit Blog'
+    
+    document.body.appendChild(element);
+    document.body.appendChild(button);
+
+    document.querySelector('.submitButton').addEventListener('click', function(){
+
+        
+        if(document.querySelector('.input').value === ''){
+            alert("The blog is empty");
+        }
+        else{
+                const button1 = document.createElement('button');
+                button1.className = '.deleteButton'
+                button1.textContent = 'Delete Blog'
+                const blogs = document.createElement('h1');
+                blogs.textContent = document.querySelector('.input').value;
+                document.body.appendChild(blogs);
+                blogs.appendChild(button1);
+
+                document.querySelector('.input').value = '';
+
+                button1.addEventListener('click', function(){
+                    console.log("The delete is clicked");
+                    blogs.remove();
+                })
+                // document.querySelector('.deleteButton').addEventListener('click', function(){
+                //     console.log("The delete is clicked");
+                // });                
+        }                
+    });    
+});
+
